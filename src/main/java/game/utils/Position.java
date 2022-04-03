6171d0adf3d9b248dc9075881cd3d4888323c8f1
Andreas Tsironis <andretsironis55@gmail.com>
2022-04-03 22:43:06 +0200

    Adding some extra functions at the Position class,in case we later have to make some more complex tasks that we will need two or more lines on code instead of one, like changing the y value and then getting the position

--- src/main/java/game/utils/Position.java
+++ src/main/java/game/utils/Position.java
@@ -35,4 +35,40 @@ public class Position {
     public void setY(int y) {
         this.y = y;
     }
-}
+
+
+
+    /**
+     * @author Andreas Tsironis
+     */
+
+
+
+        /** More complexes functions,that combine the previous functions to have them just in case they have to be used,
+         * in order to make later code to be more readable and less complex,even thought the previous functions are enough of their own.*/
+
+
+        public void setXY(int x,int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position setXGetPosition(int x) {
+            this.x = x;
+            return this;
+        }
+
+        public Position setYGetPosition(int y) {
+            this.y = y;
+            return this;
+        }
+        public Position setXYGetPosition(int x,int y) {
+            this.x = x;
+            this.y = y;
+            return this;
+        }
+
+
+
+    }
+
