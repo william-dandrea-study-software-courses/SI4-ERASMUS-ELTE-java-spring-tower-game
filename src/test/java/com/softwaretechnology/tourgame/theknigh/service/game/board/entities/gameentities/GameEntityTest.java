cd96967b66fceae37d260cd17b499a016df6dfa2
Tian Zhenman <gw9udy@inf.elte.hu>
2022-04-12 00:33:57 +0200

    Creating CastleTest; Creating GameEntityTest; Creating MonsterTest; Creating ObstacleTest;

---
+++ src/test/java/com/softwaretechnology/tourgame/theknigh/service/game/board/entities/gameentities/GameEntityTest.java
@@ -0,0 +1,13 @@
+package com.softwaretechnology.tourgame.theknigh.service.game.board.entities.gameentities;
+import com.softwaretechnology.tourgame.theknigh.service.game.utils.Position;
+import org.junit.jupiter.api.BeforeEach;
+
+public class GameEntityTest {
+
+    GameEntity gameEntity = new GameEntity(new Position(1,1));
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}
