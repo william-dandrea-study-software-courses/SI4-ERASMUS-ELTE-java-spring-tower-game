6cfc3d46b443365c360a24f484204f7cbf3b776f
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2022-04-11 08:20:00 +0200

    InitialProject

---
+++ src/main/java/com/example/theknight/TheKnightApplication.java
@@ -0,0 +1,13 @@
+package com.example.theknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheKnightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheKnightApplication.class, args);
+    }
+
+}
